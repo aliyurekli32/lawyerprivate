2a6dfbbefdc2bcdafa00b4ac59e534c70affa728
aliyurekli32 <adamjesperblack@gmail.com>
2023-03-28 13:15:46 +0300

    update:

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         <li><a>Item 3</a></li>
       </ul>
     </div>
-    <a onClick={()=>{changeNum(1)}} className="normal-case w-48 h-48"> <img src="https://github.com/aliyurekli32/lawyerprivate/blob/main/images/Logo.png"  alt="" /> </a>
+    <a onClick={()=>{changeNum(1)}} className="normal-case w-24 h-24"> <img className="w-24 h-24" src="https://github.com/aliyurekli32/lawyerprivate/blob/main/images/Logo.png"  alt="" /> </a>
   </div>
   <div className="navbar-center text-2xl hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
